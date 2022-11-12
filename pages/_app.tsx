8f99856a54bab9e8797938631fd172428ad9362a
Kir93 <kir931028@gmail.com>
2022-11-12 17:50:36 +0900

    update next font setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,15 @@ import '@styles/global.css';
 import React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
+import { Noto_Sans_KR } from '@next/font/google';
 import { ThemeProvider } from 'styled-components';
 
 import theme from '@styles/theme';
 import wrapper from '@configs/configureStore';
 
 import AppLayout from '@components/Layout/AppLayout';
+
+const notoSans = Noto_Sans_KR({ weight: '400', subsets: ['korean'] });
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <ThemeProvider theme={theme}>
@@ -18,7 +21,9 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <AppLayout>
-      <Component {...pageProps} />
+      <main className={notoSans.className}>
+        <Component {...pageProps} />
+      </main>
     </AppLayout>
   </ThemeProvider>
 );
