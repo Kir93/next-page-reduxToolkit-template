ee22d7a646361277e7f2e11f891182ab8b0f1f2b
Kir93 <kir931028@gmail.com>
2023-01-21 11:53:41 +0900

    update next fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,23 +4,45 @@ import React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { Noto_Sans_KR } from '@next/font/google';
-import { ThemeProvider } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 import theme from '@styles/theme';
 import wrapper from '@configs/configureStore';
 
 import AppLayout from '@components/Layout/AppLayout';
 
-const notoSans = Noto_Sans_KR({ weight: '400', subsets: ['korean'] });
+const notoSans = Noto_Sans_KR({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['korean'],
+  display: 'swap',
+  fallback: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Malgun Gothic',
+    'sans-serif',
+  ],
+});
+
+const GlobalStyles = createGlobalStyle`
+  html, body {
+    font-family:${notoSans.style.fontFamily};
+  }
+`;
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <ThemeProvider theme={theme}>
+    <GlobalStyles />
     <Head>
       <title>Tirrilee Next</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <AppLayout>
-      <main className={notoSans.className}>
+      <main>
         <Component {...pageProps} />
       </main>
     </AppLayout>
