cd001a98bdf27fee8c5b4844667931e86c250b77
Kir93 <kir931028@gmail.com>
2026-04-11 15:00:03 +0900

    refactor: next.config.js를 next.config.mjs로 마이그레이션

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,10 @@
 
 /** @type {import('next').NextConfig} */
 
-module.exports = {
+export default {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true
-  },
-  compiler: {
-    styledComponents: true
-  },
   images: {
     minimumCacheTTL: 31536000,
     formats: ['image/avif', 'image/webp']
-  },
-
-  webpack(config) {
-    return config;
   }
 };
